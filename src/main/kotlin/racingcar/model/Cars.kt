11af5385edb09ea60b79440aeeea2e332b54cf8c
HamBeomJoon <hbj0802@naver.com>
2025-04-14 14:07:29 +0900

    refactor: 자동차 생성시 랜덤 숫자 생성 전략 주입

--- src/main/kotlin/racingcar/model/Cars.kt
+++ src/main/kotlin/racingcar/model/Cars.kt
@@ -1,7 +1,9 @@
 package racingcar.model
 
-class Cars(val carNames: List<String>) {
-    val cars = carNames.map { name -> Car(name) }
+import racingcar.model.numberGenerator.RandomNumberGenerator
+
+class Cars(carNames: List<String>) {
+    val cars = carNames.map { name -> Car(name, RandomNumberGenerator) }
 
     init {
         require(carNames.distinct().size == carNames.size) { "자동차 이름에 중복이 있습니다." }
